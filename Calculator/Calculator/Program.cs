6d0fdff136e095d4b2024f1433922cbfde543625
MauMau24-7 <bessemarvin042@gmail.com>
2025-11-21 17:25:59 +0100

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+double x = 0;
+double y = 0;
+double result = 0;
+
+try
+{
+    Console.WriteLine("What's the first number?:");
+    x = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("What's the second number?:");
+    y = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("What basic operation to use?: (+, -, *, /)");
+    switch (Console.ReadLine())
+    {
+        case "+":
+            result = x + y;
+            Console.WriteLine($"{x} + {y} = {result}");
+            break;
+
+        case "-":
+            result = x - y;
+            Console.WriteLine($"{x} - {y} = {result}");
+            break;
+
+        case "*":
+            result = x * y;
+            Console.WriteLine($"{x} * {y} = {result}");
+            break;
+
+        case "/":
+            result = x / y;
+            Console.WriteLine($"{x} / {y} = {result}");
+            break;
+
+        default:
+            Console.WriteLine("That was not a valid option");
+            break;
+    }
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine("Fehler: " + ex);
+}
+
+
+
+Console.ReadKey();
